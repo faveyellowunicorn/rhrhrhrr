5be3b9bb7ff17b89f5fc349dd2f721b28be2f9de
faveyellowunicorn <60205248+faveyellowunicorn@users.noreply.github.com>
2022-11-01 12:59:52 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+int Sum(int x, int y) {
+	return x + y;
+}
+
+void TestSum() {
+	assert(Sum(1, 2) == 3);
+	assert(Sum(-1, -2) == -3);
+	assert(Sum(-1, 1) == 0);
+	assert(Sum(0, 1) == 1);
+	cout << "TestSum is  ok" << endl;
+
+
+}
+int main() {
+	Sum(1, 2);
+	TestSum();
+	return 0;
+
+}
